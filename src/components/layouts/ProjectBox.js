43ecaa5bf7e1cd2e1313cd552753eb52d2b462c5
Zoli Digness <zdigness@gmail.com>
2023-06-11 15:48:39 -0600

    working on project grid

--- src/components/layouts/ProjectBox.js
+++ src/components/layouts/ProjectBox.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./ProjectBox.css"
 
+import icon from '../img/background-2.png'
+
 export default function BasicBox() {
     return (
         <div className="projectbox">
-            <p className="text">Project</p>
+            <img className="project-icon" src={icon} alt="Icon"></img>
         </div>
     )
 }
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,13 +3,13 @@ import BasicBox from "../layouts/ProjectBox"
 
 export default function Home() {
     return (
-            <div className="start">
+            <div class="start">
                 <p>My Work</p>
-                <div className="grid-even-columns">
-                    <BasicBox className="box"></BasicBox>
-                    <BasicBox className="box"></BasicBox>
-                    <BasicBox className="box"></BasicBox>
-                    <BasicBox className="box"></BasicBox>
+                <div class="project-grid">
+                    <li><BasicBox className="box"></BasicBox></li>
+                    <li><BasicBox className="box"></BasicBox></li>
+                    <li><BasicBox className="box"></BasicBox></li>
+                    <li><BasicBox className="box"></BasicBox></li>
                 </div>
             </div>
     );
