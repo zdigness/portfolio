88a9990adfc6dada28cddefb4153c8e8653b08d3
Zoli Digness <zdigness@gmail.com>
2023-09-18 19:40:08 -0600

    implement emailjs for form submition

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -1,13 +1,28 @@
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
+
 import "./ContactPage.css"
 
 export default function ContactPage() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.REACT_APP_SERVICE, process.env.REACT_APP_TEMPLATE, form.current, process.env.REACT_APP_PUBLIC_KEY)
+      .then((result) => {
+        window.location.replace("/contact-submission");
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
     return (
-      <div class="contact">
+      <div className="contact">
         <h1>Contact Me</h1>
-        <form className="contact-form" action="https://formsubmit.co/a8b8b67a97e6426843e5b5c58b70cd5a" method="POST">
-        <input type="hidden" name="_next" value="http://localhost:3000/contact-submission"></input>
-        <input type="text" class="form-input" name="name" placeholder="Your Name"/>
-        <input type="email" class="form-input" name="email" placeholder="Email Address"/>
+        <form ref={form} onSubmit={sendEmail}>
+        <input type="text" class="form-input" name="user_name" placeholder="Your Name"/>
+        <input type="email" class="form-input" name="user_email" placeholder="Email Address"/>
         <textarea type="text" class="message-input" name="message" placeholder="Message"/>
         <input type="submit" class="submit-button" value="Submit" />
         </form>
