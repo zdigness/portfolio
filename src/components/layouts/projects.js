039c28ac94c07a756267e09fe52068e997be3835
Zoli Digness <zdigness@gmail.com>
2024-04-02 16:37:43 -0600

    Update CSS styles and add new project layout

--- src/components/layouts/projects.js
+++ src/components/layouts/projects.js
@@ -9,23 +9,23 @@ import mysql from '../img/mysql.png'
 
 export default function BasicBox() {
     return (
-        <div className="main-project">
-            <div className="projectmain">
-                <div className="projectbox">
-                <img className="project-icon" src={icon} alt="Icon"></img>
+        <div className="projects">
+            <div className="project-layout">
+                <div className="leftside">
+                    <div className="number">
+                        <h2>01</h2>
+                    </div>
+                    <div className="name">
+                        <h1>Puttdle</h1>
+                    </div>
+                    <div className="description">
+                        <p>2D Daily Golf Game</p>
+                    </div>
+                </div>
+                <div className="rightside">
+                    <img src={django} alt="Icon"></img>
                 </div>
             </div>
-            <div className="projectdisc">
-                <div>
-                <p class="budget-app">Budgeting app to track monthly expenses, calculate savings, and manage debts.</p>
-                </div>
-                <div class="tech-stack">
-                <img className="tech-icon" src={django} alt="Icon"></img>
-                <img className="tech-icon" src={python} alt="Icon"></img>
-                <img className="tech-icon" src={mysql} alt="Icon"></img>
-                <a href="https://github.com/zdigness/personal_finance"><button type="button" class="btn btn-primary">GitHub</button></a>
-                </div>
-                </div>
         </div>
     )
 }
